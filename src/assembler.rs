7ba0cd1b809f6efd7ad96f9b4727ab4eb8a06d4e
Rich Lane <lanerl@gmail.com>
2017-02-07 20:54:39 -0800

    initial assembler

---
+++ src/assembler.rs
@@ -0,0 +1,51 @@
+// Copyright 2017 Rich Lane <lanerl@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0> or
+// the MIT license <http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+
+//! This module translates eBPF assembly language to instructions.
+
+use asm_parser::{Instruction, parse};
+use ebpf;
+use ebpf::Insn;
+use std::collections::HashMap;
+
+fn instruction_table() -> Vec<(String, u8)> {
+    vec![("exit".to_string(), ebpf::BPF_EXIT)]
+}
+
+fn assemble_one(instruction: &Instruction,
+                instruction_map: &HashMap<String, u8>)
+                -> Result<Insn, String> {
+    match instruction_map.get(&instruction.name) {
+        Some(opc) => {
+            Ok(Insn {
+                opc: *opc,
+                dst: 0,
+                src: 0,
+                off: 0,
+                imm: 0,
+            })
+        }
+        None => Err("Invalid instruction".to_string()),
+    }
+}
+
+fn assemble_internal(instructions: &[Instruction]) -> Result<Vec<Insn>, String> {
+    let instruction_map: HashMap<String, u8> = instruction_table().iter().cloned().collect();
+    let mut result = vec![];
+    for instruction in instructions {
+        match assemble_one(instruction, &instruction_map) {
+            Ok(insn) => result.push(insn),
+            Err(msg) => return Err(msg),
+        }
+    }
+    Ok(result)
+}
+
+/// XXX
+pub fn assemble(src: &str) -> Result<Vec<Insn>, String> {
+    assemble_internal(&try!(parse(src)))
+}
