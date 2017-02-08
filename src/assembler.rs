904246615288a8946ca1636d8def97a1863fc478
Rich Lane <lanerl@gmail.com>
2017-02-07 21:26:41 -0800

    support add64 reg and imm

--- src/assembler.rs
+++ src/assembler.rs
@@ -7,22 +7,43 @@
 
 //! This module translates eBPF assembly language to instructions.
 
-use asm_parser::{Instruction, parse};
+use asm_parser::{Instruction, Operand, parse};
 use ebpf;
 use ebpf::Insn;
 use std::collections::HashMap;
 
 fn instruction_table() -> Vec<(String, u8)> {
-    vec![("exit".to_string(), ebpf::BPF_EXIT)]
+    vec![("exit".to_string(), ebpf::BPF_EXIT),
+         ("add64".to_string(), ebpf::BPF_ALU64 | ebpf::BPF_ADD)]
 }
 
-fn encode(opc: u8) -> Result<Insn, String> {
+fn encode(opc: u8, operands: &Vec<Operand>) -> Result<Insn, String> {
+    let mut opc: u8 = opc;
+    let mut dst: u8 = 0;
+    let mut src: u8 = 0;
+    let mut imm: i32 = 0;
+    let mut seen_dst = false;
+    for operand in operands {
+        match operand {
+            &Operand::Register(x) => {
+                if seen_dst {
+                    src = x as u8;
+                    opc |= ebpf::BPF_X;
+                } else {
+                    dst = x as u8;
+                    seen_dst = true;
+                }
+            }
+            &Operand::Integer(x) => imm = x as i32,
+            _ => panic!("unexpected operand"),
+        }
+    }
     Ok(Insn {
         opc: opc,
-        dst: 0,
-        src: 0,
+        dst: dst,
+        src: src,
         off: 0,
-        imm: 0,
+        imm: imm,
     })
 }
 
@@ -30,7 +51,7 @@ fn assemble_one(instruction: &Instruction,
                 instruction_map: &HashMap<String, u8>)
                 -> Result<Insn, String> {
     match instruction_map.get(&instruction.name) {
-        Some(opc) => encode(*opc),
+        Some(opc) => encode(*opc, &instruction.operands),
         None => Err("Invalid instruction".to_string()),
     }
 }
